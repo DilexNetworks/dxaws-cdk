2b558a2c971106ae8400ee93ce7de53c672c5ead
wyllie <wyllie@dilex.net>
2024-11-18 16:55:53 -0500

    Bump version: 1.0.4 → 1.0.5

--- docs/conf.py
+++ docs/conf.py
@@ -27,10 +27,10 @@ copyright = '2024, Dilex Networks'
 author = 'Andrew Wyllie'
 
 # The full version, including alpha/beta/rc tags
-release = 'v1.0.4'
+release = 'v1.0.5'
 
 # Set the title
-html_title = "DxAws CDK Components v1.0.4"
+html_title = "DxAws CDK Components v1.0.5"
 
 
 # -- General configuration ---------------------------------------------------
